36e5579a210aaa3d9cfb40882d98e65d28c2f150
Iryna Mekh <iryna.mekh@gmail.com>
2023-09-08 14:00:56 +0300

    added validation to CreateBookRequestDto class

--- src/main/java/com/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/bookstore/dto/CreateBookRequestDto.java
@@ -2,15 +2,19 @@ package com.bookstore.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 import org.hibernate.validator.constraints.ISBN;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 public class CreateBookRequestDto {
     @NotNull
+    @Size(min = 1, max = 255)
     private String title;
     @NotNull
+    @Size(min = 1, max = 255)
     private String author;
     @NotNull
     @ISBN
@@ -19,5 +23,6 @@ public class CreateBookRequestDto {
     @Min(value = 0)
     private BigDecimal price;
     private String description;
+    @URL
     private String coverImage;
 }
--- src/main/java/com/bookstore/model/Book.java
+++ src/main/java/com/bookstore/model/Book.java
@@ -30,6 +30,6 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
-    @Column(nullable = false, name = "is_deleted")
+    @Column(nullable = false)
     private boolean isDeleted = false;
 }
